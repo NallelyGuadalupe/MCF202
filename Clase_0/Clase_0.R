26ff27ef0d76be756a16bef825d148de09b1c033
Nallely Aguirre <nallely.aguirreojd@uanl.edu.mx>
2019-08-05 15:34:07 -0500

    Clase_R

---
+++ Clase_0/Clase_0.R
@@ -0,0 +1,30 @@
+#Nallely Aguirre
+#05/08/2019
+#Clase 0
+
+# pasos básicos -----------------------------------------------------------
+
+2+2
+a <- 2
+
+a * a
+a + 5
+
+
+# importar datos ----------------------------------------------------------
+
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+
+diametro
+
+#Medidas de tendencia central
+
+mean(diametro)
+median(diametro)
+
+#Medidas de disperción
+
+sd(diametro)
+var(diametro)
