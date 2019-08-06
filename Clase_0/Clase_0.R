fb91dce73377d694929e591f45e97ad0fc4523c7
unknown <nallely.aguirreojd@uanl.edu.mx>
2019-08-06 08:47:31 -0500

    Clase_0Completa

--- Clase_0/Clase_0.R
+++ Clase_0/Clase_0.R
@@ -28,3 +28,33 @@ median(diametro)
 
 sd(diametro)
 var(diametro)
+# Importar datos ----------------------------------------------------------
+
+DB_alturas <- read.csv("C:/MCF202-2019/MCF202/Datos/alturas.csv")
+boxplot(DB_alturas$crecimiento)
+boxplot(DB_alturas$crecimiento ~ DB_alturas$tratamiento)
+boxplot(DB_alturas$crecimiento ~ DB_alturas$tratamiento, 
+        col="lightgreen",
+        xlab= "tratamiento",
+        ylab = "crecimiento (cm)",
+        main = "efectos del fertilizante")
+
+mean(DB_alturas$crecimiento)
+
+
+# Restricciones -----------------------------------------------------------
+
+sum(DB_alturas$crecimiento < mean(DB_alturas$crecimiento))
+TratA <- DB_alturas[!(DB_alturas$tratamiento == "TA"),]
+
+mean(TratA$crecimiento)
+
+
+
+# Submuestra --------------------------------------------------------------
+
+T.mean <- subset(DB_alturas,crecimiento >= mean(DB_alturas$crecimiento))
+
+boxplot(T.mean$crecimiento ~ T.mean$tratamiento)
+
+
