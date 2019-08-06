1a4f5b9f1a65962462c96c2b0c11b45ad2c50124
Nallely Aguirre <nallely.aguirreojd@uanl.edu.mx>
2019-08-06 15:22:07 -0500

    Clase_2

---
+++ Clase_2/Clase_2.R
@@ -0,0 +1,9 @@
+#Nallely Aguirre
+#06/08/2019
+#Clase_2
+
+
+# Importar datos vivero ---------------------------------------------------
+
+vivero <- read.csv("C:/MCF202-2019/MCF202/Datos/tvivero.csv", header = T)
+summary(vivero)
